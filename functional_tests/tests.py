c7acbe0ca9b1c1807ed587717234436811cc1d12
ism <sangmin.imm@gmail.com>
2017-03-24 01:30:26 +0900

    레이아웃과  스타일링을 위한 FT 첫단계

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -101,3 +101,14 @@ class NewVisitorTest(LiveServerTestCase):
         # 둘다 만족하고 끝난다.
 
         # self.fail('Finish the test!')
+
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        inputbox = self.browser.find_element_by_id('item_text')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
